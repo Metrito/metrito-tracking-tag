dc8133816681dd9114ed048319d0c5272408ff41
Matheus Batista <matheusb.comp@gmail.com>
2024-10-01 17:54:25 -0300

    wip: configurable triggers

--- index.js
+++ index.js
@@ -1,4 +1,60 @@
+// https://gist.github.com/matheusb-comp/040183787c587fb9b65bd6853c9afe28
+
+/**
+ * Reads the entire response body and process based in the Content-Type header
+ * https://developer.mozilla.org/en-US/docs/Web/API/Body
+ * 
+ * @param {object}  response    A response from a fetch request
+ * @param {boolean} arrayBuffer When body can't be processed to `text`,
+ * `json`, or `form-data`, process it to an ArrayBuffer or a Blob (default)
+ * 
+ * @return {Promise} A promise that resolves after the entire body is processed
+ */
+function processBody(response, arrayBuffer = false) {
+  const mime = (response.headers.get('Content-Type') || '').split(',')[0]
+  if (mime.includes('text')) return response.text()
+  else if (mime.includes('json')) return response.json()
+  else if (mime.includes('form-data')) return response.formData()
+  else return (arrayBuffer) ? response.arrayBuffer() : response.blob()
+}
+
+/**
+ * Requests a resource using fetch, and process the response body by default
+ * 
+ * @param {string}  resource     URL of a network resource
+ * @param {object}  options      Fetch request options object
+ * @param {boolean} readBody     Read and process the entire response body
+ * @param {boolean} arrayBuffer  When body can't be processed to `text`,
+ * `json`, or `form-data`, process it to an ArrayBuffer or a Blob (default)
+ * 
+ * @return {Promise} A promise that resolves when the request is successful
+ * (HTTP status code between 200 and 299) and rejects otherwise.
+ * Errors are similar to the Axios library, with `error.response` (HTTP errors)
+ * and `error.message` (failed to fetch / cancelled requests)
+ */
+function request(resource, options, readBody = true, arrayBuffer = false) {
+  return new Promise((resolve, reject) => {
+    const config = { resource, ...options }
+    fetch(resource, options)
+      // Reject if not ok (HTTP status not in the range 200-299)
+      .then((response) => {
+        const message = 'Request failed'
+        const res = { response, config }
+        // If requested, read and process the entire response body
+        if (readBody) processBody(response, arrayBuffer).then((data) => {
+          return (response.ok)
+            ? resolve({ ...res, data })
+            : reject({ ...res, data, message })
+        })
+        else (response.ok) ? resolve(res) : reject({ ...res, message })
+      })
+      // Network/Cancel errors, similar to the error handling in Axios
+      .catch((error) => reject({ message: error.message, config }))
+  })
+}
+
 // Inicializa o tracking quando a página for carregada
+
 (async function () {
   console.log("Tracking initialized with container:", window.location.host);
 
@@ -227,18 +283,51 @@
       this.backendUrl = "https://tracking.metrito.com";
       this.ipInfoToken = "BB9F053F0108613A9CCD7D8DF0B21A47";
       this.fbPixelChecker = new FacebookPixelChecker();
+      this.conversions = [];
     }
 
     async init() {
       // Verifica se o Facebook Pixel foi carregado antes de continuar
       await this.fbPixelChecker.checkPixelAndCookies();
-
       this.leadId = await this.getOrCreateLeadId();
       this.utmHandler = new UtmHandler(this.leadId);
       this.utmHandler.updateUrl();
+
+      // Configura todos os triggers configurados para esta página
+      const url = "https://api.metrito.com/v2/tracking/conversions/" + this.containerDomain;
+      (await this.getData(url) || []).forEach((conversion) => this.setupTrigger(conversion));
+
+      // Configura outros eventos
       this.trackPageView();
       this.injectLeadIdOnHiddenInput("form_fields[lead_id]");
       document.addEventListener("submit", (ev) => this.handleFormSubmit(ev));
+    }
+
+    // TODO: Trazer script com todos os dados pré-carregados (evitar GET)
+    getData(url) {
+      return request(url, {
+        method: "GET",
+        mode: "cors",
+        credentials: "omit",
+      }).then((data) => {
+        Logger.info("Data retrieved:", data);
+        return data;
+      }).catch((error) => {
+        Logger.error("Error retrieving data:", error);
+      });
+    }
+
+    // TODO: Substituir fetch por sendBeacon (para POST JSON)
+    sendData(url, data) {
+      request(url, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(data),
+      }).then(() => {
+        Logger.info(`Data sent to ${url}:`, data);
+      }).catch((error) => {
+        Logger.error(`Error sending data to ${url}:`, error);
+      });
     }
 
     // TODO: Buscar configurações do backend customizadas para o lead
@@ -266,19 +355,7 @@
         metaAds: { cookies: { fbc, fbp } },
         geolocation: { ...geolocation, state: geolocation.region },
       };
-
-      try {
-        const response = await fetch(`${this.backendUrl}/leads`, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(leadData),
-        });
-        if (!response.ok) throw new Error(`Failed to create lead: ${response.statusText}`);
-        Logger.info("Lead successfully created", leadData);
-        Storage.setJsonItem("geolocation", geolocation);
-      } catch (error) {
-        Logger.error("Error creating lead:", error);
-      }
+      this.sendData(`${this.backendUrl}/leads`, leadData);
     }
 
     async getOrCreateLeadId() {
@@ -305,19 +382,13 @@
 
     async getGeolocation() {
       let geolocation = Storage.getJsonItem("geolocation");
-      if (!geolocation) {
-        try {
-          const response = await fetch(`https://ipinfo.io/json?key=${this.ipInfoToken}`);
-          geolocation = await response.json();
-          Logger.info("Geolocation retrieved:", geolocation);
-        } catch (error) {
-          Logger.error("Error retrieving geolocation:", error);
-          return {};
-        }
-      } else {
+      if (geolocation) {
         Logger.info("Geolocation retrieved from localStorage:", geolocation);
+        return geolocation;
       }
-      return geolocation;
+      geolocation = await this.getData(`https://ipinfo.io/json?key=${this.ipInfoToken}`);
+      if (geolocation) Storage.setJsonItem("geolocation", geolocation);
+      return geolocation || {};
     }
 
     async trackEvent(eventType, eventData = {}) {
@@ -341,23 +412,7 @@
         metaAds: { cookies: { fbc, fbp } },
         geolocation: { ...geolocation, state: geolocation.region },
       };
-
-      Logger.info(`Tracking event: ${eventType}`, data);
-      await this.sendEventToBackend(data);
-    }
-
-    async sendEventToBackend(data) {
-      try {
-        const response = await fetch(`${this.backendUrl}/events`, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(data),
-        });
-        if (!response.ok) throw new Error(`Failed to send event data: ${response.statusText}`);
-        Logger.info("Event data successfully sent to backend", data);
-      } catch (error) {
-        Logger.error("Error sending event data to backend:", error);
-      }
+      this.sendData(`${this.backendUrl}/events`, data);
     }
 
     trackPageView() {
@@ -394,6 +449,89 @@
 
       // Envia dados encontrados para o backend
       if (Object.keys(data).length > 0) this.trackEvent("Lead", data);
+    }
+
+    setupTrigger(conversion) {
+      const {
+        eventType,
+        customEventName,
+        loadOn,
+        specificPages,
+        triggerConfig,
+        eventConfig,
+      } = conversion;
+      // TODO: Simplificar nome do evento (usar apenas 1 campo)
+      const eventName = eventType === "CustomEvent" ? customEventName : eventType;
+      
+      // Verifica se o trigger deve ser ignorado para este path
+      const path = window.location.pathname;
+      if (!loadOn) return;
+      else if (loadOn === "specific_pages") {
+        if (!specificPages?.includes(path)) return;
+      } else if (loadOn === "regex") {
+        const regex = new RegExp(specificPages?.[0] || '^$');
+        if (!regex.test(path)) return;
+      }
+      Logger.info("Configuring trigger for conversion:", conversion);
+      
+      // Se for possível, configura o trigger
+      switch (triggerConfig.triggerType) {
+        case "page_view":
+          return this.setupTriggerTimeout(0, eventName, eventConfig);
+        case "page_view_duration":
+          const delay = triggerConfig.timeThreshold || 0;
+          return this.setupTriggerTimeout(delay, eventName, eventConfig);
+        case "scroll_depth":
+          const pct = triggerConfig.scrollPercentage || 0;
+          return this.setupTriggerWindowScroll(pct, eventName, eventConfig);
+        case "element_click":
+          const selector = triggerConfig.elementSelector;
+          return this.setupTriggerElementEvent(selector, 'click', eventName, eventConfig);
+        case "element_hover":
+          const selector = triggerConfig.elementSelector;
+          return this.setupTriggerElementEvent(selector, 'mouseover', eventName, eventConfig);
+        case "element_view":
+          const selector = triggerConfig.elementSelector;
+          const opt = triggerConfig.elementObserverOptions;
+          return this.setupTriggerElementObserver(selector, opt, eventName, eventConfig);
+      }
+    }
+
+    setupTriggerTimeout(delay, eventName, eventData = {}) {
+      setTimeout(() => this.trackEvent(eventName, eventData), delay);
+    }
+
+    setupTriggerWindowScroll(pct, eventName, eventData = {}) {
+      const handler = () => {
+        // Usando "scrollHeight" em vez de "offsetHeight"
+        // https://css-tricks.com/how-i-put-the-scroll-percentage-in-the-browser-title-bar
+        // https://medium.com/@jbbpatel94/difference-between-offsetheight-clientheight-and-scrollheight-cfea5c196937
+        const scrollPercentage = 100 * (
+          window.scrollY / (document.body.scrollHeight - window.innerHeight)
+        );
+        if (scrollPercentage >= pct) {
+          window.removeEventListener('scroll', handler);
+          this.trackEvent(eventName, { ...eventData, scrollPercentage });
+        }
+      };
+      window.addEventListener('scroll', handler);
+    }
+
+    setupTriggerElementEvent(selector, domEvent, eventName, eventData = {}) {
+      document.querySelectorAll(selector).forEach((el) => {
+        el.addEventListener(domEvent, () => this.trackEvent(eventName, eventData));
+      });
+    }
+
+    setupTriggerElementObserver(selector, obsOptions, eventName, eventData = {}) {
+      const obs = new IntersectionObserver((entries, obs) => {
+        entries.forEach((observerEntry) => {
+          if (!observerEntry.isIntersecting) return;
+          obs.unobserve(observerEntry.target);
+          this.trackEvent(eventName, { ...eventData, observerEntry });
+        });
+      }, obsOptions);
+      document.querySelectorAll(selector).forEach((el) => obs.observe(el));
     }
   }
 
